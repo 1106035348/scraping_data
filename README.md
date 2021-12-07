9cc3d90dd300de115189b1b8a2b18e66c3e073f6
Aaron <yilei@hotcoin.biz>
2021-12-07 18:06:31 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scraping_data
+
 区块链咨询抓取，主要来源为非小号、cmc
---
+++ src/test/java/com/data/scrapingdata/ScrapingDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.scrapingdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
